17e88a6ae02ba2e9265d60b269d189b20d40d151
pankaj-p-atel <pankaj73qq@gmail.com>
2025-05-17 20:04:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 
 try:
-    model = load_model("potato_model.keras", compile=False)
+    model = load_model("potato_model.h5", compile=False)
     print(" Model loaded successfully.")
 except Exception as e:
     print("Failed to load model:", e)
